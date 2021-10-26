fb5f773cc60ac37604cf22341b1ffdd3bd874acd
Lachezar Kutsarov <lkutsarov@gmail.com>
2021-10-26 10:04:16 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,24 @@
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+
+    const frequency = new Array(LETTERS_IN_ALPHABET);
+    frequency.fill(0);
+
+    let n = magazine.length;
+    for (let i = 0; i < n; i++) {
+        frequency[magazine.codePointAt(i) - ASCII_CODE_LOWERCASE_A]++;
+    }
+
+    n = ransomNote.length;
+    for (let i = 0; i < n; i++) {
+        if (--frequency[ransomNote.codePointAt(i) - ASCII_CODE_LOWERCASE_A] < 0) {
+            return false;
+        }
+    }
+    return true;
+};
